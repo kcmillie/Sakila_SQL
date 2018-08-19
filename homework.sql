7e19d48a6e1532dc0b7df819b4270cf018fe6748
ckruder <carmenkchan@gmail.com>
2018-08-19 15:34:47 -0500

    did more stuff

--- homework.sql
+++ homework.sql
@@ -34,3 +34,76 @@ order by last_name, first_name;
 select country_id, country
 from country
 where country IN ('Afghanistan', 'Bangladesh','China')
+
+-- 3a. Add a middle_name column to the table actor. Position it between first_name and last_name.
+-- Hint: you will need to specify the data type.
+alter table actor
+add middle_name varchar(50) AFTER first_name;
+
+-- 3b. You realize that some of these actors have tremendously long last names.
+-- Change the data type of the middle_name column to blobs.
+alter table actor
+modify column middle_name blob;
+
+-- 3c. Now delete the middle_name column.
+alter table actor
+drop column middle_name;
+
+-- 4a. List the last names of actors, as well as how many actors have that last name.
+select last_name, count(last_name)
+from actor
+group by last_name;
+
+-- 4b. List last names of actors and the number of actors who have that last name,
+-- but only for names that are shared by at least two actors
+select last_name, count(last_name)
+from actor
+group by last_name
+having count(last_name) > 2;
+
+-- 4c. Oh, no! The actor HARPO WILLIAMS was accidentally entered in the actor table as
+-- GROUCHO WILLIAMS, the name of Harpo's second cousin's husband's yoga teacher. Write a query to fix the record.
+UPDATE actor
+SET first_name = 'HARPO'
+where first_name = 'GROUCHO' AND last_name = 'WILLIAMS';
+
+-- 4d. Perhaps we were too hasty in changing GROUCHO to HARPO.
+-- It turns out that GROUCHO was the correct name after all! In a single query, if the first name of the actor
+-- is currently HARPO, change it to GROUCHO. Otherwise, change the first name to MUCHO GROUCHO, as that is
+-- exactly what the actor will be with the grievous error. BE CAREFUL NOT TO CHANGE THE FIRST NAME OF EVERY
+-- ACTOR TO MUCHO GROUCHO, HOWEVER! (Hint: update the record using a unique identifier.)
+
+UPDATE actor
+SET first_name =
+    CASE
+        WHEN first_name = 'HARPO'
+        THEN 'GROUCHO'
+        ELSE 'MUCHO GROUCHO'
+    END
+WHERE actor_id = 172;
+
+-- 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
+SHOW CREATE TABLE address;
+
+CREATE TABLE `address` (
+  `address_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `address` varchar(50) NOT NULL,
+  `address2` varchar(50) DEFAULT NULL,
+  `district` varchar(20) NOT NULL,
+  `city_id` smallint(5) unsigned NOT NULL,
+  `postal_code` varchar(10) DEFAULT NULL,
+  `phone` varchar(20) NOT NULL,
+  `location` geometry NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`address_id`),
+  KEY `idx_fk_city_id` (`city_id`),
+  SPATIAL KEY `idx_location` (`location`),
+  CONSTRAINT `fk_address_city` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=606 DEFAULT CHARSET=utf8;
+
+
+
+
+
+
+
